c28c3c6b785cdad3d20657ae5a79b55f2438b3f8
Jim Strother <jim.strother@gmail.com>
2019-04-20 13:49:16 -0500

    changed font color; other changes to still make

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
-import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import store from './store';
 
-Vue.use(Vuelidate);
 Vue.config.productionTip = false;
 
 new Vue({
